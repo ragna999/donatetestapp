218b49dda63e176ba97bb203a16a9035f518d708
ragna999 <erikmaulana123qwert@gmail.com>
2025-07-31 03:13:14 -0500

    small update

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         {/* Logo */}
         <Link href="/" className="text-2xl font-extrabold text-blue-700 tracking-tight">
-          🌐 DonasiChain
+        🌱Donatree
         </Link>
 
         {/* Hamburger button */}
